53d64152fb005e4f9f9b7f9d81b1229fd498c213
Alex Rock Ancelet <pierstoval@gmail.com>
2020-06-10 22:51:59 +0200

    Added better ejs support for routes

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { Application } from "https://deno.land/x/abc@v1.0.0-rc10/mod.ts";
 import { renderFile } from "https://deno.land/x/dejs/mod.ts";
+import { readFileStrSync } from "https://deno.land/std@v0.55.0/fs/mod.ts";
 
 const PORT = 8000;
 
@@ -8,8 +9,11 @@ console.log(`http://127.0.0.1:${PORT}/`);
 const app = new Application();
 
 app.renderer = {
-    render<T>(name: string, data: T): Promise<Deno.Reader> {
-        return renderFile(name, data);
+    render<T>(name: string, data: any): Promise<Deno.Reader> {
+        const template_name = './public/index.html';
+        data.view_content = readFileStrSync(`./views/${name}.ejs`);
+
+        return renderFile(template_name, data);
     },
 };
 
@@ -24,7 +28,7 @@ app
     .static('/vendor', './public/vendor')
     .get("/", async (c) => {
         console.log(`Resolved index.`);
-        await c.render('./public/index.html');
+        await c.render('index', {});
     })
     .start({
         port: PORT
